c94e10640baa2f528fb33b942b9d86a45821b7ab
Alvorak <58303198+Alvorak@users.noreply.github.com>
2025-03-30 17:18:13 +0200

    Create RMutua.cpp

---
+++ EDA/Recursividad/RMutua.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+//Para poder usar acentos en la consola
+#include <Windows.h>
+
+//Written and Directed By Álvaro García Herrero
+
+/*Recursividad Multiple: Implica que más de una función se
+llaman mutuamente.
+*/
+
+using namespace std;
+
+int par(int n);
+int impar(int n);
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8); //Con esto permito acentos en la consola
+
+    int n;
+    
+    cout << "Escribe un numero" << endl;
+    cin >> n;
+    if(par(n) == 0){
+        cout << "El numero " << n << ") es par" << endl;
+    }else{
+        cout << "El numero " << n << ") es impar" << endl;
+    }
+  
+    return 0;
+}
+
+int par(int n){
+  if(n==0){
+    return 1;
+  }else{
+    return impar(n-1);
+  }
+}
+
+int par(int n){
+  if(n==0){
+    return 0;
+  }else{
+    return par(n-1);
+  }
+}
